418e3c74e2c4d9a8fd9096cebb727c9c1d65839d
Zain Ali <122364970+Zynalex9@users.noreply.github.com>
2025-11-12 10:32:01 +0500

    Refactor HelpSection for better layout and styling
    Updated HelpSection layout for improved responsiveness and styling.

--- src/components/home/Help.tsx
+++ src/components/home/Help.tsx
@@ -3,8 +3,15 @@ import { useTranslations } from "next-intl";
 
 export default function HelpSection() {
   const t = useTranslations("HomePage");
-  return <div className="space-y-2">
-    <h1 className="text-2xl font-medium mt-4 px-3">{t("help-title")}</h1>
-    <p className="text-md text-center ">{t("help-sub-text")}</p>
-  </div>;
+
+  return (
+    <div className="space-y-2 px-6 sm:px-8 md:px-0 text-center md:text-left">
+      <h1 className="text-xl sm:text-2xl md:text-3xl font-medium mt-4">
+        {t("help-title")}
+      </h1>
+      <p className="text-sm sm:text-md md:text-base text-gray-700">
+        {t("help-sub-text")}
+      </p>
+    </div>
+  );
 }
